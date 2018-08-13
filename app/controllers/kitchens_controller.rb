5b40ac696e6fbe826961c4ef3a0e8e455ace467d
Celine Te <celine.te.mcgill@gmail.com>
2018-08-13 17:57:17 -0400

    Kitchens changes

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -1,2 +1,34 @@
 class KitchensController < ApplicationController
+
+  def index
+    if params[:query].present?
+      @query = params[:query]
+      @kitchens = Kitchen.where("name iLike '%#{params[:query]}%'")
+    else
+      @kitchens = Kitchen.all
+    end
+  end
+
+  def new
+    @kitchen = Kitchen.new
+  end
+
+  def create
+    @ = Kitchen.new(kitchen_params)
+    if @kitchen.save
+      redirect_to kitchens_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @kitchen = Kitchen.find(params[:id])
+  end
+
+  private
+
+  def kitchen_params
+    params.require(:kitchen).permit(:title, :address, :photo, :description, :amenities, :price, :rating, :capacity, :availability)
+  end
 end
