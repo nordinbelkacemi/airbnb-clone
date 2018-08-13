149e7bb5ce18235107091fe990765cf44d4972af
Nordin Belkacemi <belkaceminordin@gmail.com>
2018-08-13 16:26:24 -0400

    changed start and end time in bookings to just a day

--- db/migrate/20180813182859_create_bookings.rb
+++ db/migrate/20180813182859_create_bookings.rb
@@ -1,9 +1,8 @@
 class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
-      t.datetime :start_time
-      t.datetime :end_time
-
+      t.datetime :day
+      
       t.references :user, foreign_key: true
       t.references :kitchen, foreign_key: true
       t.timestamps
