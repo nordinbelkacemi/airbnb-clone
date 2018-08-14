1e989db21a680c2138cd2a3c951d99dfeabd5f83
Celine Te <celine.te.mcgill@gmail.com>
2018-08-14 11:01:19 -0400

    Index feature done

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -3,32 +3,42 @@ class KitchensController < ApplicationController
   def index
     if params[:query].present?
       @query = params[:query]
-      @kitchens = Kitchen.where("name iLike '%#{params[:query]}%'")
+      @kitchens = Kitchen.where("title iLike '%#{params[:query]}%'")
     else
       @kitchens = Kitchen.all
     end
   end
 
-  def new
-    @kitchen = Kitchen.new
-  end
+  # def new
+  #   @kitchen = Kitchen.new
+  # end
 
-  def create
-    @ = Kitchen.new(kitchen_params)
-    if @kitchen.save
-      redirect_to kitchens_path
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @kitchen = Kitchen.new(kitchen_params)
+  #   if @kitchen.save
+  #     redirect_to kitchens_path
+  #   else
+  #     render :new
+  #   end
+  # end
 
-  def show
-    @kitchen = Kitchen.find(params[:id])
-  end
+  # # def show
+#   # #   @kitchen = Kitchen.find(params[:id])
+#   # end
 
-  private
+#   private
 
-  def kitchen_params
-    params.require(:kitchen).permit(:title, :address, :photo, :description, :amenities, :price, :rating, :capacity, :availability)
-  end
+#   def kitchen_params
+#     params.require(:kitchen).permit(:title, :address, :photo, :description, :amenities, :price, :rating, :capacity, :availability)
+#   end
 end
+
+
+
+
+
+
+
+
+
+
