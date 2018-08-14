27f9cd8af87a954f7815ab462538509866938603
Kojo Addaquay <kojoaddaquay@gmail.com>
2018-08-14 11:03:41 -0400

    fixed migration issues and added booking index

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   # root to: '/kitchens'
   resources :kitchens
-  resources :bookings, except: [:index]
+  resources :bookings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
