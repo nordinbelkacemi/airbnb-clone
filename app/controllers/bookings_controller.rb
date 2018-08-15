f915ef7c53e9ca114bff781eb19ae2bff9eaa2e5
Nordin Belkacemi <belkaceminordin@gmail.com>
2018-08-15 08:36:47 -0400

    booking (almost there)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,14 @@
 class BookingsController < ApplicationController
-  before_action :find_booking, only: [:show, :edit, :update, :destroy]
-
   def new
+    @kitchen = Kitchen.find(params[:kitchen_id])
     @booking = Booking.new
   end
 
   def create
-    @booking = Booking.new(params[:booking])
-    @booking.save
+    raise
+    @booking = Booking.new(booking_params)
+    @booking.save!
+    redirect_to kitchens_path
   end
 
   def show
@@ -28,8 +29,14 @@ class BookingsController < ApplicationController
 
   private
 
-  def find_booking
-    @booking = Booking.find(params[:id])
+  def booking_params
+    prms = params.require(:booking).permit("day(1i)", "day(2i)", "day(3i)")
+    date = DateTime.parse("#{prms['day(1i)']}/#{prms['day(2i)']}/#{prms['day(3i)']}")
+    return {
+      day: date,
+      user_id: current_user.id,
+      kitchen_id: kitchen_id,
+      created_at: DateTime.now
+    }
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "kitchens#index"
   devise_for :users
-  # root to: '/kitchens'
-  resources :kitchens
-  resources :bookings, except: [:index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :kitchens do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, except: [:new, :create]
 end
