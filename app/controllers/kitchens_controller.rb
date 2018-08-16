6e627bc42f7a54624cb17cb3ee160e7585cd8b4f
Nordin Belkacemi <belkaceminordin@gmail.com>
2018-08-16 16:14:02 -0400

    added links to delete and update, + controller actions but it's not working yet

--- app/controllers/kitchens_controller.rb
+++ app/controllers/kitchens_controller.rb
@@ -1,5 +1,6 @@
 class KitchensController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+  before_action :set_kitchen, only: [:edit, :update, :destroy]
 
   def index
     if current_user == nil
@@ -26,12 +27,29 @@ class KitchensController < ApplicationController
     end
   end
 
+  def update
+    @kitchen.update(kitchen_params_update)
+    redirect_to profile_path
+  end
+
+  def destroy
+    @kitchen.destroy
+    redirect_to profile_path
+  end
+
   private
 
   def kitchen_params
     prms = params.require(:kitchen).permit(:title, :description, :address, :photo, :capacity, :amenities, :price)
     prms[:user_id] = current_user.id
     prms[:created_at] = DateTime.now
+    return prms
+  end
+
+  def kitchen_params_update
+    prms = params.require(:kitchen).permit(:title, :description, :address, :photo, :capacity, :amenities, :price)
+    prms[:user_id] = current_user.id
+    prms[:updated_at] = DateTime.now
     return prms
   end
 end
