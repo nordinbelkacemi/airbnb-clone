62c9afbccba17ce0d7dba5072b57ca1b0cf949ee
Nordin Belkacemi <belkaceminordin@gmail.com>
2018-08-17 10:02:55 -0400

    click on avatar of the kitchen card -> view profile

--- db/migrate/20180813181200_create_users.rb
+++ db/migrate/20180813181200_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :username
       t.string :photo
+      t.string :bio
+      t.string :from
     end
   end
 end
